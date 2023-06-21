46185dfb87111ccf23d609bfa363bbf6565c7e62
Wenderson Leite da Silva <wender_leite@hotmail.com>
2023-06-21 16:23:04 -0300

    Inclusão dos componentes
    Inclusão dos componentes de cadastro e pagina do usuário.

--- controle-de-agenda/src/app/app.module.ts
+++ controle-de-agenda/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { PaginaDoUsuarioComponent } from './pagina-do-usuario/pagina-do-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent,
+    PaginaDoUsuarioComponent
   ],
   imports: [
     BrowserModule,
